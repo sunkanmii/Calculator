181fafe724ed63d8e2bedf6394fc4ee71b252331
Fafowora Olasunkanmi <sunkanmifafowora@gmail.com>
2019-06-26 11:40:41 +0100

    All buttons working

--- calculator.js
+++ calculator.js
@@ -16,21 +16,30 @@ let j = 1;
 let removedNum;
 let newNums;
 let num1, num2, ans, newNum1;
+let addButton = 0, subButton = 0, mulButton = 0, divButton = 0; 
+
 
 function clearField() {
     output_vals.value = 0;
+    nums = [[,],[,]];
+    num1 = 0;
+    num2 = 0;
+    ans = 0;
+    i = 0; 
+    j = 1;
+    addButton = 0, subButton = 0, mulButton = 0, divButton = 0;
 }
 
 function deleteField() {
     
     for (; i < j; i++) {   
-        nums[i] = output_vals.value.split("")
+        nums[i] = output_vals.value.split("");
     }
 
     removedNum = nums[i - 1].pop();
     console.log("You just removed " + removedNum);
 
-    console.log(i)
+    console.log(i);
     console.log(nums[i - 1]);
 
     //Save new numbers back
@@ -59,12 +68,22 @@ function AddValTxtbox(id) {
 }
 
 function addNums(){
-    
+
+    if(subButton > 0){
+        subNums();
+    }
+    else if(mulButton > 0){
+        mulNums();
+    }
+    else if(divButton > 0){
+        divNums();
+    }
+
     for (; i < j; i++) {   
         nums[i] = output_vals.value.split("");
         output_vals.placeholder = output_vals.value;    
     }
-
+    
     output_vals.value = ""; 
 
     j++;
@@ -76,7 +95,7 @@ function addNums(){
         for(; y > i; y--){
             num1 = Number(nums[i - 2].join("")); 
             console.log(num1);
-
+            
             num2 = Number(nums[i - 1].join(""));
             console.log(num2);
             
@@ -95,10 +114,204 @@ function addNums(){
         
         output_vals.placeholder = output_vals.value; 
         output_vals.value = "";
-         
+        
         i = 1;
         j = 2;
     }
+
+    addButton++;
+    subButton = 0;
+    mulButton = 0;
+    divButton = 0;
+}
+
+function subNums(){
+
+    if(addButton > 0){
+        addNums();
+    }
+    else if(mulButton > 0){
+        mulNums();
+    }
+    else if(divButton > 0){
+        divNums();
+    }
+    
+    for (; i < j; i++) {
+        nums[i] = output_vals.value.split("");
+        output_vals.placeholder = output_vals.value;    
+    }
+    
+    output_vals.value = ""; 
+
+    j++;
+    
+    let y = i + 1;
+    
+    if(i > 1){
+        
+        for(; y > i; y--){
+            
+            num1 = Number(nums[i - 2].join("")); 
+            console.log(num1);
+            
+            num2 = Number(nums[i - 1].join(""));
+            console.log(num2);
+            
+            ans = num1 - num2;
+            console.log(String(ans));
+        }
+        
+        nums[i - 2] = ans.toString().split("");
+        
+        console.log(nums);
+        
+        //same as nums[0]
+        newNum1 = nums[i - 2].join("");
+        
+        output_vals.value = Number(newNum1);
+        
+        output_vals.placeholder = output_vals.value; 
+        output_vals.value = "";
+        
+        i = 1;
+        j = 2;
+    }
+
+    addButton = 0;
+    subButton++;
+    mulButton = 0;
+    divButton = 0;    
+}
+
+function mulNums(){
+    
+    if(addButton > 0){
+        addNums();
+        i = 1;
+        j = 1;
+    }
+    else if(subButton > 0){
+        subNums();
+        i = 1;
+        j = 1;
+    }
+    else if(divButton > 0){
+        divNums();
+        i = 1;
+        j = 1;
+    }
+    
+
+    for (; i < j; i++) {   
+        nums[i] = output_vals.value.split("");
+        
+        output_vals.placeholder = output_vals.value;    
+    }
+    
+    output_vals.value = ""; 
+
+    j++;
+    
+    let y = i + 1;
+    
+    if(i > 1){
+        
+        for(; y > i; y--){
+            num1 = Number(nums[i - 2].join("")); 
+            console.log(num1);
+            
+            num2 = Number(nums[i - 1].join(""));
+            console.log(num2);
+            
+            ans = num1 * num2;
+            console.log(String(ans));
+        }
+        
+        nums[i - 2] = ans.toString().split("");
+        
+        console.log(nums);
+        
+        //same as nums[0]
+        newNum1 = nums[i - 2].join("");
+        
+        output_vals.value = Number(newNum1);
+        
+        output_vals.placeholder = output_vals.value; 
+        output_vals.value = "";
+        
+        i = 1;
+        j = 2;
+    }
+
+    addButton = 0;
+    subButton = 0;
+    mulButton++;
+    divButton = 0;
+}
+
+function divNums(){
+
+    if(addButton > 0){
+        addNums();
+        i = 1;
+        j = 1;
+    }
+    else if(subButton > 0){
+        subNums();
+        i = 1;
+        j = 1;
+    }
+    else if(mulButton > 0){
+        mulNums();
+        i = 1;
+        j = 1;
+    }
+
+    for (; i < j; i++) {   
+        nums[i] = output_vals.value.split("");
+        output_vals.placeholder = output_vals.value;    
+    }
+    
+    output_vals.value = ""; 
+
+    j++;
+    
+    let y = i + 1;
+    
+    if(i > 1){
+        
+        for(; y > i; y--){
+            num1 = Number(nums[i - 2].join("")); 
+            console.log(num1);
+            
+            num2 = Number(nums[i - 1].join(""));
+            console.log(num2);
+            
+            ans = num1 / num2;
+            console.log(String(ans));
+        }
+        
+        nums[i - 2] = ans.toString().split("");
+        
+        console.log(nums);
+        
+        //same as nums[0]
+        newNum1 = nums[i - 2].join("");
+        
+        output_vals.value = Number(newNum1);
+        
+        output_vals.placeholder = output_vals.value; 
+        output_vals.value = "";
+        
+        i = 1;
+        j = 2;
+    }
+
+    addButton = 0;
+    subButton = 0;
+    mulButton = 0;
+    divButton++;
 }
 
 function equalTo(){
@@ -112,15 +325,25 @@ function equalTo(){
     console.log(num1);
     console.log(num2);
 
-    ans = num1 + num2;
+    if(addButton > 0){
+        ans = num1 + num2;
+    }
+    else if(subButton > 0){
+        ans = num1 - num2;
+    }
+    else if(mulButton > 0){
+        ans = num1 * num2;
+    }
+    else if(divButton > 0){
+        ans = num1 / num2;
+    }
 
     nums[i - 1] = ans.toString().split("");
-        
+    
     console.log(nums);
-        
+    
     //same as nums[0]
     newNum1 = nums[i - 1].join("");
 
     output_vals.value = Number(newNum1);
-
 }
